ed6119e02640e027de0e4326cb9f6e3de8bd7354
Glitch (hello-express) <none>
2019-05-22 16:09:31 +0000

    ☺️🐁 Checkpoint ./server.js:4411779/1832

--- server.js
+++ server.js
@@ -1,29 +1,46 @@
 const express = require("express");
-const cors = require('cors')
-
 const app = express();
-
+var cors = require('cors');
+app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
 const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!"
+  from: "Clement",
+  text: "Welcome to Freeborn chat system!",
+  id: 0
 }
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage]
+//const { check, validationResult } = require('express-validator/check')
 
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
+// // this will get the array of all messages
+app.get("/messages", function(request, response){
+  response.send(messages)
+})
 
+// // this will get the array of message by id only
+app.get("/messages/:id", function(request, response){
+  const id =request.params.id
+  const myMessageId =messages.filter(message=> message.id ==id)
+  response.json(myMessageId);
+});
 
-
-
+app.post("/messages", function(request, response){
+  const welcomeMessage =request.body
+  //const { Name, Message } = request.body
+  welcomeMessage.id= messages.length;
+  messages.push(welcomeMessage);
+  response.status(201).json(welcomeMessage);
+  fromInput.focus();
+});
+//this is to validate the text field
 
 app.listen(process.env.PORT);
