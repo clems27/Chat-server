e38a96bd78f6d5b2039b9c797e5f73c8c9383f22
Glitch (hello-express) <none>
2019-05-30 17:55:31 +0000

    🕍🚝 Checkpoint ./server.js:4411779/1222 ./index.html:4411779/62

--- server.js
+++ server.js
@@ -16,7 +16,78 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage]
+const messages = [
+    {
+        from: "Clement",
+        text: "Welcome to Freeborn chat system!",
+        id: 0
+    },
+    {
+        from: "mark",
+        text: "Hello",
+        id: 1
+    },
+    {
+        from: "clement",
+        text: "welcome",
+        id: 2
+    },
+    {
+        from: "mark",
+        text: "long time",
+        id: 3
+    },
+    {
+        from: "clement",
+        text: "yeah, indeed",
+        id: 4
+    },
+    {
+        from: "mark",
+        text: "real good to be hear",
+        id: 5
+    },
+    {
+        from: "clement",
+        text: "you looking good",
+        id: 6
+    },
+    {
+        from: "mark",
+        text: "Hello",
+        id: 7
+    },
+    {
+        from: "clement",
+        text: "welcome",
+        id: 8
+    },
+    {
+        from: "mark",
+        text: "long time",
+        id: 9
+    },
+    {
+        from: "clement",
+        text: "yeah, indeed",
+        id: 10
+    },
+    {
+        from: "clement",
+        text: "welcome",
+        id: 11
+    },
+    {
+        from: "mark",
+        text: "long time",
+        id: 12
+    },
+    {
+        from: "clement",
+        text: "yeah, indeed",
+        id: 13
+    }
+]
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
@@ -28,7 +99,7 @@ app.get('/', function(request, response) {
  })
 app.get("/messages/latest", function(request, response){ 
   //const latest =messages
-  response.json(messages)
+  response.json(messages.slice(-2))
 })
   // this will get the array of message by id only
  app.get("/messages/:id", function(request, response){
