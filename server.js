aebe06f7093f4fcda66d349d19a8890957f526d3
Glitch (hello-express) <none>
2019-05-31 00:15:30 +0000

    🛩👩‍👩‍👦 Checkpoint ./server.js:4411779/188

--- server.js
+++ server.js
@@ -132,13 +132,13 @@ app.get("/messages/latest", function(request, response){
 
 app.put("/messages/:id", function(request, response){
    const id =request.params.id
-   const newMessage =request.body;
-   const myMessage =messages.filter(message=> message.id ==id);
-  //   newMessage.from =myMessage.from
+   const update =request.body;
+   const updateMessage =messages.filter(message=> message.id ==id);
+    if()
   // newMessage.text =myMessage.text
     //newMessage.id = myMessage.id
   //  newMessage.timeStamp = myMessage.timeStamp
-  response.json(myMessage)
+  response.json(updateMessage)
   
 })
 // app.delete("/messages/:id", function(request, response){
